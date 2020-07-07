6c957aeab5e9cc24fa7176df65493ff2703acfd5
Igor Lukanin <me@igorlukanin.io>
2020-07-07 16:25:07 +0300

    New contest website with landing page and registration

---
+++ precache-manifest.8cfd724ad18151d4e53c3410e68d2b9d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33adbc9b90cd39439b49b541ac61cd02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49f54742571782bae87d",
+    "url": "/static/css/main.5fee6615.chunk.css"
+  },
+  {
+    "revision": "25dcc2d1c70d0511f5ad",
+    "url": "/static/js/2.b3094b64.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.b3094b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f54742571782bae87d",
+    "url": "/static/js/main.40047ba9.chunk.js"
+  },
+  {
+    "revision": "b1baaab392094ac8b8a9",
+    "url": "/static/js/runtime-main.e62d9830.js"
+  },
+  {
+    "revision": "98878a7fb6c4d11f006f5f0a48816bae",
+    "url": "/static/media/Kontur-Iconic-eco-20px.98878a7f.woff2"
+  },
+  {
+    "revision": "b811ce6cc66ff93ba6ba6dd225b475df",
+    "url": "/static/media/Kontur-Iconic-eco-20px.b811ce6c.eot"
+  },
+  {
+    "revision": "cc8e65cb2d3a893285466730d7b4f022",
+    "url": "/static/media/Kontur-Iconic-eco-20px.cc8e65cb.woff"
+  },
+  {
+    "revision": "7d850235c4b0a12aed1c0d83e489acd5",
+    "url": "/static/media/fallback_circle.7d850235.png"
+  },
+  {
+    "revision": "7890347bd63a549c5164f53487b8031f",
+    "url": "/static/media/fallback_circle_big.7890347b.png"
+  },
+  {
+    "revision": "dae3133efdcd89ac9e4d3335d2675b1d",
+    "url": "/static/media/fallback_circle_normal.dae3133e.png"
+  },
+  {
+    "revision": "7b359e7d9365a987d17524f81fc66fb5",
+    "url": "/static/media/font.7b359e7d.woff2"
+  },
+  {
+    "revision": "dc62e0fc93a9b5469860b6a22338562d",
+    "url": "/static/media/font.dc62e0fc.eot"
+  },
+  {
+    "revision": "f01e8d1506e8cb4cbb1f1c39dab9dab9",
+    "url": "/static/media/font.f01e8d15.woff"
+  }
+]);
