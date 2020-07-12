77db4b5d6e92e98d749891512b23b1e0ae50ea1b
aminopyrodin <aminopyrodin@gmail.com>
2020-07-13 01:56:41 +0500

    update statuses and time format

--- precache-manifest.2e8f74bd096767410b0101f224bf52bd.js
+++ precache-manifest.2e8f74bd096767410b0101f224bf52bd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8059f47b66cb13a2cfe77b38cc3dee90",
+    "revision": "9359232e2fe934c7a5f90a874eb22ffc",
     "url": "/index.html"
   },
   {
-    "revision": "7076ca7cd7ac417796b0",
+    "revision": "5fe30d5afcd24579ea5d",
     "url": "/static/css/main.d4d22c1e.chunk.css"
   },
   {
-    "revision": "c553e3f60038a3dcabaf",
-    "url": "/static/js/2.71ab6896.chunk.js"
+    "revision": "82b5872256abf6efcba4",
+    "url": "/static/js/2.7a4c4def.chunk.js"
   },
   {
     "revision": "b05eac5c9c8a1e887114ca05ef6407d4",
-    "url": "/static/js/2.71ab6896.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7a4c4def.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7076ca7cd7ac417796b0",
-    "url": "/static/js/main.8aab832c.chunk.js"
+    "revision": "5fe30d5afcd24579ea5d",
+    "url": "/static/js/main.689941ac.chunk.js"
   },
   {
     "revision": "b1baaab392094ac8b8a9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e91a88144257ad33a445c68d0dcf55e6.js"
+  "/precache-manifest.2e8f74bd096767410b0101f224bf52bd.js"
 );
 
 self.addEventListener('message', (event) => {
